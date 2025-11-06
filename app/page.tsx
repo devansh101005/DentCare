7dfce8779290dc11701171fad98a4b916b5d99fa
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-11-06 16:18:50 +0530

    making home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,22 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { SignUpButton,SignedOut,SignedIn,SignOutButton } from "@clerk/nextjs";
+import Hero from "@/components/landing/Hero";
+import Header from "@/components/landing/Header";
+import HowItWorks from "@/components/landing/HowItWorks";
+import PricingSection from "@/components/landing/PricingSection";
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
 
 export default function Home() {
   return (
-    <div>
-    <h1>
-      Home Page
-    </h1>
-
-    <SignedOut>
-    <SignUpButton mode ="modal">SignUp</SignUpButton>
-    </SignedOut>
-
-    <SignedIn>
-      <SignOutButton>
-        LogOut
-      </SignOutButton>
-    </SignedIn>
-
-
+    <div className="min-h-screen bg-background"> 
+    <Header />
+    <Hero />
+    <HowItWorks />
+    <PricingSection />
+    <CTA />
+    <Footer />
     </div>
   )
 }
