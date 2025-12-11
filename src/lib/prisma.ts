11c8ad8a0f619539f9633a731f5251a845cd5bc2
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-12-11 21:18:22 +0530

    migrated from prisma 5 to prisma 7 yayyyy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,24 @@
-// create a prisma instance and cache in development
+// // create a prisma instance and cache in development
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// Above code isnt supported by prisma 6 
+
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient(); // no adapter needed
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
